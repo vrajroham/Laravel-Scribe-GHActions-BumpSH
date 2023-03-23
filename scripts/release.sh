e414d0bec72e8137d41dbd09c86f7191a6ad0ec8
Vaibhavraj Roham <vaibhav.roham@urbanpiper.com>
2023-03-24 03:41:13 +0530

    Updated release script (#14)

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,15 +7,30 @@ set -e
 repo_owner="vrajroham"
 repo_name="Laravel-Scribe-GHActions-BumpSH"
 
+echo ""
+echo -e "\033[31;47m1. Checking out to master branch\033[0m"
 # Checkout the master branch
 git checkout master
 
+# ensure git working directory is clean
+if [ -z "$(git status --porcelain)" ]; then
+  echo ""
+else
+  echo -e "\033[31;47m Working directory not clean. Commit or stash changes first \033[0m"
+
+  exit 2
+fi
+
+echo ""
+echo -e "\033[31;47m2. Pulling latest from master branch\033[0m"
 # Pull the latest changes from the master branch
 git pull origin master
 
 # Add a blank line
 echo "------------------------------------------------------------"
 
+echo ""
+echo -e "\033[31;47m3. Select the release type\033[0m"
 # Ask the user if it's a major or minor release
 read -p "Is this a major or minor release? (major/minor): " release_type
 
@@ -37,32 +52,41 @@ fi
 # Update the version number in the config file
 awk -v new_version="$new_version" -F\' '/version/ {OFS=FS; $4=new_version} 1' config/prime.php > temp && mv temp config/prime.php
 
-echo "Updated version to $new_version"
+echo ""
+echo -e "\033[31;47mNew version number: $new_version\033[0m"
 
 # Add a blank line
 echo "------------------------------------------------------------"
 
 # Run the tests
-echo "Running tests..."
+echo ""
+echo -e "\033[31;47m4. Running tests\033[0m"
 ./vendor/bin/phpunit
 
 # Add a blank line
 echo "------------------------------------------------------------"
 
 # Commit and push the changes
-echo "Committing changes..."
+echo ""
+echo -e "\033[31;47m5. Committing changes\033[0m"
 git add config/prime.php
 git commit -m "Release $new_version"
 
 # Add a blank line
 echo "------------------------------------------------------------"
 
+echo ""
+echo -e "\033[31;47m6. Pushing changes to upstream master branch\033[0m"
 # Push the changes to the master branch
 git push origin master
 
+echo ""
+echo -e "\033[31;47m7. Checkout to production branch\033[0m"
 # Checkout the production branch
 git checkout production
 
+echo ""
+echo -e "\033[31;47m8. Pull latest from production branch\033[0m"
 # Pull the latest changes from the production branch
 git pull origin production
 
@@ -82,13 +106,15 @@ pull_request_number=$(gh pr list --base production --head master --repo "$repo_o
 if [ -n "$pull_request_number" ]; then
 
   # Update the body and title of the existing pull request
-  echo "Updating the body of the existing pull request..."
+  echo ""
+  echo -e "\033[31;47m9. Updating the body of the existing pull request. Number: $pull_request_number\033[0m"
   gh pr edit "$pull_request_number" --title "$pull_request_title" --body "$pull_request_body" --repo "$repo_owner/$repo_name"
 
 else
 
     # Create a pull request using the `gh` command
-    echo "Creating a pull request..."
+    echo ""
+    echo -e "\033[31;47m9. Creating new pull request\033[0m"
     gh pr create --title "$pull_request_title" --body "$pull_request_body" --base production --head master --repo "$repo_owner/$repo_name"
 
 fi
@@ -98,5 +124,8 @@ git checkout master
 
 # Add a blank line
 echo "------------------------------------------------------------"
+
+echo ""
+echo -e "\033[42m\033[1;37m Done! Checkout the PR on github.com \033[0m"
 
 
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo -e "\e[1;31mThis text will be red\e[0m"
+echo -e "\033[48;5;208m This text has a background color of orange! \033[0m"
+
+echo -e "\033[31;47mRed text on white background\033[0m"
+
 
